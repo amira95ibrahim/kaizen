4d472ec850df5864f9081491dea66e5f8461357b
menna <fullaloka38@gmail.com>
2021-09-27 00:12:21 +0200

    register and login

--- kaizen/backend/resources/views/auth/register.blade.php
+++ kaizen/backend/resources/views/auth/register.blade.php
@@ -61,6 +61,20 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="phone_number" class="col-md-4 col-form-label text-md-right">{{ __('phone number') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="phone_number" type="phone_number" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" required autocomplete="new-phone_number">
+
+                                @error('phone_number')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
